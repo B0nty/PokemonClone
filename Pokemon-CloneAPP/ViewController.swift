7856becfb8cae0ca2d8f86d14c3bb2a998b154da
Home-bl0b <B0nty-iMac27@b0ntys-imac.local>
2017-04-13 17:06:53 +0200

    Initial Commit

---
+++ Pokemon-CloneAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon-CloneAPP
+//
+//  Created by B0nty on 13/04/2017.
+//  Copyright © 2017 B0nty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
